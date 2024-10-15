8d2bf5c1fc8c4e5137a7d552b0291a97f5c0dd20
2003harsh <harshnkgupta@gmail.com>
2024-10-15 11:55:29 +0530

    Added Stage 2

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -4,16 +4,21 @@ from sklearn.model_selection import train_test_split
 
 import os
 
+#downloading raw data
 df = pd.read_csv('https://raw.githubusercontent.com/campusx-official/jupyter-masterclass/main/tweet_emotions.csv')
 
+#applying some basic changes
 df.drop(columns=['tweet_id'],inplace=True)
 
 final_df = df[df['sentiment'].isin(['neutral','sadness'])]
 
 final_df['sentiment'].replace({'neutral':1, 'sadness':0},inplace=True)
 
+#train_test_split
 train_data, test_data = train_test_split(final_df, test_size=0.2, random_state=42)
 
+
+#storing data in data/raw
 data_path=os.path.join("data","raw")
 
 os.makedirs(data_path)
